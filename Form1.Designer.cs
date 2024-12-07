a5e9ed52e0d527c859de5c2b5d7988be03ebc570
Amaury Cheung <amaury@outlook.jp>
2024-12-07 17:15:13 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace YOLODetectionApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxInput = new System.Windows.Forms.PictureBox();
+            this.pictureBoxOutput = new System.Windows.Forms.PictureBox();
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.numericUpDownConfidence = new System.Windows.Forms.NumericUpDown();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnLoadConfig = new System.Windows.Forms.Button();
+            this.BtnLoadWeights = new System.Windows.Forms.Button();
+            this.BtnLoadClassNames = new System.Windows.Forms.Button();
+            this.txtConfigPath = new System.Windows.Forms.TextBox();
+            this.txtWeightsPath = new System.Windows.Forms.TextBox();
+            this.txtClassNamesPath = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOutput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownConfidence)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxInput
+            // 
+            this.pictureBoxInput.Location = new System.Drawing.Point(12, 180);
+            this.pictureBoxInput.Name = "pictureBoxInput";
+            this.pictureBoxInput.Size = new System.Drawing.Size(1024, 673);
+            this.pictureBoxInput.TabIndex = 0;
+            this.pictureBoxInput.TabStop = false;
+            // 
+            // pictureBoxOutput
+            // 
+            this.pictureBoxOutput.Location = new System.Drawing.Point(1042, 180);
+            this.pictureBoxOutput.Name = "pictureBoxOutput";
+            this.pictureBoxOutput.Size = new System.Drawing.Size(1024, 673);
+            this.pictureBoxOutput.TabIndex = 1;
+            this.pictureBoxOutput.TabStop = false;
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(886, 124);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(150, 50);
+            this.btnSelectImage.TabIndex = 2;
+            this.btnSelectImage.Text = "选择图像";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.BtnSelectImage_Click);
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(1042, 124);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(150, 50);
+            this.btnDetect.TabIndex = 3;
+            this.btnDetect.Text = "开始检测";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.BtnDetect_Click);
+            // 
+            // numericUpDownConfidence
+            // 
+            this.numericUpDownConfidence.Location = new System.Drawing.Point(1198, 149);
+            this.numericUpDownConfidence.Name = "numericUpDownConfidence";
+            this.numericUpDownConfidence.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDownConfidence.TabIndex = 4;
+            this.numericUpDownConfidence.Tag = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1232, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "置信度";
+            // 
+            // BtnLoadConfig
+            // 
+            this.BtnLoadConfig.Location = new System.Drawing.Point(12, 11);
+            this.BtnLoadConfig.Name = "BtnLoadConfig";
+            this.BtnLoadConfig.Size = new System.Drawing.Size(150, 50);
+            this.BtnLoadConfig.TabIndex = 6;
+            this.BtnLoadConfig.Text = "选择配置文件";
+            this.BtnLoadConfig.UseVisualStyleBackColor = true;
+            this.BtnLoadConfig.Click += new System.EventHandler(this.BtnLoadConfig_Click);
+            // 
+            // BtnLoadWeights
+            // 
+            this.BtnLoadWeights.Location = new System.Drawing.Point(12, 68);
+            this.BtnLoadWeights.Name = "BtnLoadWeights";
+            this.BtnLoadWeights.Size = new System.Drawing.Size(150, 50);
+            this.BtnLoadWeights.TabIndex = 7;
+            this.BtnLoadWeights.Text = "选择权重文件";
+            this.BtnLoadWeights.UseVisualStyleBackColor = true;
+            this.BtnLoadWeights.Click += new System.EventHandler(this.BtnLoadWeights_Click);
+            // 
+            // BtnLoadClassNames
+            // 
+            this.BtnLoadClassNames.Location = new System.Drawing.Point(12, 124);
+            this.BtnLoadClassNames.Name = "BtnLoadClassNames";
+            this.BtnLoadClassNames.Size = new System.Drawing.Size(150, 50);
+            this.BtnLoadClassNames.TabIndex = 8;
+            this.BtnLoadClassNames.Text = "选择类别文件";
+            this.BtnLoadClassNames.UseVisualStyleBackColor = true;
+            this.BtnLoadClassNames.Click += new System.EventHandler(this.BtnLoadClassNames_Click);
+            // 
+            // txtConfigPath
+            // 
+            this.txtConfigPath.Location = new System.Drawing.Point(168, 26);
+            this.txtConfigPath.Name = "txtConfigPath";
+            this.txtConfigPath.Size = new System.Drawing.Size(482, 25);
+            this.txtConfigPath.TabIndex = 9;
+            // 
+            // txtWeightsPath
+            // 
+            this.txtWeightsPath.Location = new System.Drawing.Point(168, 83);
+            this.txtWeightsPath.Name = "txtWeightsPath";
+            this.txtWeightsPath.Size = new System.Drawing.Size(482, 25);
+            this.txtWeightsPath.TabIndex = 10;
+            // 
+            // txtClassNamesPath
+            // 
+            this.txtClassNamesPath.Location = new System.Drawing.Point(168, 139);
+            this.txtClassNamesPath.Name = "txtClassNamesPath";
+            this.txtClassNamesPath.Size = new System.Drawing.Size(482, 25);
+            this.txtClassNamesPath.TabIndex = 11;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2095, 869);
+            this.Controls.Add(this.txtClassNamesPath);
+            this.Controls.Add(this.txtWeightsPath);
+            this.Controls.Add(this.txtConfigPath);
+            this.Controls.Add(this.BtnLoadClassNames);
+            this.Controls.Add(this.BtnLoadWeights);
+            this.Controls.Add(this.BtnLoadConfig);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownConfidence);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.btnSelectImage);
+            this.Controls.Add(this.pictureBoxOutput);
+            this.Controls.Add(this.pictureBoxInput);
+            this.Name = "MainForm";
+            this.Text = "VisionInspection";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOutput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownConfidence)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxInput;
+        private System.Windows.Forms.PictureBox pictureBoxOutput;
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.NumericUpDown numericUpDownConfidence;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnLoadConfig;
+        private System.Windows.Forms.Button BtnLoadWeights;
+        private System.Windows.Forms.Button BtnLoadClassNames;
+        private System.Windows.Forms.TextBox txtConfigPath;
+        private System.Windows.Forms.TextBox txtWeightsPath;
+        private System.Windows.Forms.TextBox txtClassNamesPath;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,339 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Dnn;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace YOLODetectionApp
+{
+    public partial class MainForm : Form
+    {
+        // 存储文件路径的变量
+        private string modelConfig;
+        private string modelWeights;
+        private string classNamesFile;
+
+        private string[] classNames;
+        private Net net;
+        private Mat inputImage;
+        private bool modelLoaded = false; // 用于判断模型是否已经加载
+
+        // 置信度阈值，默认设置为0.5
+        private float confidenceThreshold = 0.5f;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeControls();
+                // 设置路径文本框为只读
+    txtConfigPath.ReadOnly = true;
+    txtWeightsPath.ReadOnly = true;
+    txtClassNamesPath.ReadOnly = true;
+        }
+
+        // 初始化控件设置
+        private void InitializeControls()
+        {
+            numericUpDownConfidence.Value = (decimal)confidenceThreshold;
+            numericUpDownConfidence.Minimum = 0;
+            numericUpDownConfidence.Maximum = 1;
+            numericUpDownConfidence.DecimalPlaces = 2;
+            numericUpDownConfidence.Increment = 0.01M;
+            numericUpDownConfidence.ValueChanged += (sender, e) =>
+            {
+                // 更新置信度阈值
+                confidenceThreshold = (float)numericUpDownConfidence.Value;
+            };
+        }
+
+        // 加载YOLO模型
+        private void LoadYOLOModel()
+        {
+            // 检查是否需要重新加载模型
+            if (IsModelLoaded())
+            {
+                return;
+            }
+
+            // 加载类别文件
+            if (!LoadClassNames())
+            {
+                return;
+            }
+
+            try
+            {
+                // 加载网络
+                net = CvDnn.ReadNetFromDarknet(modelConfig, modelWeights);
+                if (net.Empty())
+                {
+                    MessageBox.Show("YOLO模型加载失败，请检查配置和权重文件！");
+                    return;
+                }
+
+                net.SetPreferableBackend(Backend.OPENCV);
+                net.SetPreferableTarget(Target.CPU);
+
+                modelLoaded = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"加载YOLO模型时发生错误：{ex.Message}");
+            }
+        }
+
+        // 检查模型是否已经加载且路径没有变化
+        private bool IsModelLoaded()
+        {
+            return modelLoaded && modelConfig == txtConfigPath.Text && modelWeights == txtWeightsPath.Text && classNamesFile == txtClassNamesPath.Text;
+        }
+
+        // 加载类别文件
+        private bool LoadClassNames()
+        {
+            if (string.IsNullOrEmpty(classNamesFile) || !File.Exists(classNamesFile))
+            {
+                MessageBox.Show("类别文件未找到，请检查路径！");
+                return false;
+            }
+
+            try
+            {
+                classNames = File.ReadAllLines(classNamesFile);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"读取类别文件时发生错误：{ex.Message}");
+                return false;
+            }
+
+            return true;
+        }
+
+        // 选择图像并显示
+        private void BtnSelectImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "图像文件|*.jpg;*.jpeg;*.png;*.bmp;*.webp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    inputImage = Cv2.ImRead(openFileDialog.FileName);
+
+                    // 获取图片和PictureBox的大小
+                    int pictureBoxWidth = pictureBoxInput.Width;
+                    int pictureBoxHeight = pictureBoxInput.Height;
+
+                    // 调整图像大小以适应PictureBox
+                    Mat resizedImage = ResizeImage(inputImage, pictureBoxWidth, pictureBoxHeight);
+
+                    // 将调整后的图像显示在PictureBox中
+                    pictureBoxInput.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(resizedImage);
+                }
+            }
+        }
+
+        // 检测图像中的物体
+        private void BtnDetect_Click(object sender, EventArgs e)
+        {
+            // 检查配置文件和图像是否有效
+            if (string.IsNullOrEmpty(modelConfig) || string.IsNullOrEmpty(modelWeights) || string.IsNullOrEmpty(classNamesFile))
+            {
+                MessageBox.Show("配置文件、权重文件或类别文件尚未加载，请先选择文件！");
+                return;
+            }
+
+            if (inputImage.Empty())
+            {
+                MessageBox.Show("请先选择一张图像！");
+                return;
+            }
+
+            // 加载模型（如果还未加载）
+            LoadYOLOModel();
+
+            // 进行物体检测
+            var detectedImage = DetectObjects(inputImage);
+
+            // 获取图片和PictureBox的大小
+            int pictureBoxWidth = pictureBoxOutput.Width;
+            int pictureBoxHeight = pictureBoxOutput.Height;
+
+            // 调整图像大小以适应PictureBox
+            Mat resizedImage = ResizeImage(detectedImage, pictureBoxWidth, pictureBoxHeight);
+
+            // 将调整后的图像显示在PictureBox中
+            pictureBoxOutput.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(resizedImage);
+        }
+        // 图像缩放方法
+        private Mat ResizeImage(Mat image, int maxWidth, int maxHeight)
+        {
+            // 计算缩放比例
+            double aspectRatio = (double)image.Width / image.Height;
+            int newWidth = maxWidth;
+            int newHeight = (int)(maxWidth / aspectRatio);
+
+            // 如果新高度超过PictureBox高度，调整宽度和高度
+            if (newHeight > maxHeight)
+            {
+                newHeight = maxHeight;
+                newWidth = (int)(maxHeight * aspectRatio);
+            }
+
+            // 使用OpenCV进行图像缩放
+            Mat resizedImage = new Mat();
+            Cv2.Resize(image, resizedImage, new Size(newWidth, newHeight));
+            return resizedImage;
+        }
+
+        // 选择并加载模型配置文件
+        private void BtnLoadConfig_Click(object sender, EventArgs e)
+        {
+            // 记录之前的路径
+            string previousConfigPath = modelConfig;
+
+            // 选择文件
+            modelConfig = LoadFile("配置文件|*.cfg");
+
+            // 如果文件选择不是取消（路径不为空），才更新文本框和变量
+            if (!string.IsNullOrEmpty(modelConfig))
+            {
+                txtConfigPath.Text = modelConfig;
+            }
+            else
+            {
+                // 如果点击取消，恢复之前的路径
+                modelConfig = previousConfigPath;
+            }
+        }
+
+        // 选择并加载模型权重文件
+        private void BtnLoadWeights_Click(object sender, EventArgs e)
+        {
+            // 记录之前的权重文件路径
+            string previousWeightsPath = modelWeights;
+
+            // 选择文件
+            modelWeights = LoadFile("权重文件|*.weights");
+
+            // 如果文件选择不是取消（路径不为空），才更新文本框和变量
+            if (!string.IsNullOrEmpty(modelWeights))
+            {
+                txtWeightsPath.Text = modelWeights;
+            }
+            else
+            {
+                // 如果点击取消，恢复之前的路径
+                modelWeights = previousWeightsPath;
+            }
+        }
+
+        // 选择并加载类别文件
+        private void BtnLoadClassNames_Click(object sender, EventArgs e)
+        {
+            // 记录之前的类别文件路径
+            string previousClassNamesPath = classNamesFile;
+
+            // 选择文件
+            classNamesFile = LoadFile("类别文件|*.names");
+
+            // 如果文件选择不是取消（路径不为空），才更新文本框和变量
+            if (!string.IsNullOrEmpty(classNamesFile))
+            {
+                txtClassNamesPath.Text = classNamesFile;
+            }
+            else
+            {
+                // 如果点击取消，恢复之前的路径
+                classNamesFile = previousClassNamesPath;
+            }
+        }
+
+        // 文件加载通用方法
+        private string LoadFile(string filter)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = filter;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return openFileDialog.FileName;
+                }
+            }
+            return null;
+        }
+
+        // 检测物体
+        private Mat DetectObjects(Mat image)
+        {
+            int width = image.Width;
+            int height = image.Height;
+
+            // 图像预处理
+            var blob = CvDnn.BlobFromImage(image, 1.0 / 255.0, new Size(416, 416), new Scalar(0, 0, 0), true, false);
+            net.SetInput(blob);
+
+            // 获取输出层
+            var outputLayerNames = net.GetUnconnectedOutLayersNames();
+            var outputs = outputLayerNames.Select(name => new Mat()).ToArray();
+
+            // 前向传播
+            net.Forward(outputs, outputLayerNames);
+
+            // 解析检测结果
+            var boxes = new List<Rect>();
+            var confidences = new List<float>();
+            var classIds = new List<int>();
+
+            foreach (var output in outputs)
+            {
+                for (int i = 0; i < output.Rows; i++)
+                {
+                    float[] detection = new float[output.Cols];
+                    Marshal.Copy(output.Ptr(i), detection, 0, output.Cols);
+
+                    float confidence = detection[4];
+                    if (confidence > confidenceThreshold) // 置信度阈值
+                    {
+                        int centerX = (int)(detection[0] * width);
+                        int centerY = (int)(detection[1] * height);
+                        int boxWidth = (int)(detection[2] * width);
+                        int boxHeight = (int)(detection[3] * height);
+                        int x = centerX - boxWidth / 2;
+                        int y = centerY - boxHeight / 2;
+
+                        boxes.Add(new Rect(x, y, boxWidth, boxHeight));
+                        confidences.Add(confidence);
+
+                        float[] scores = detection.Skip(5).ToArray();
+                        classIds.Add(Array.IndexOf(scores, scores.Max()));
+                    }
+                }
+            }
+
+            // 使用 NMSBoxes 进行非极大值抑制
+            CvDnn.NMSBoxes(
+                boxes.ToArray(),
+                confidences.ToArray(),
+                confidenceThreshold,  // 置信度阈值
+                0.4f,   // NMS 阈值
+                out int[] indices
+            );
+
+            // 绘制检测框
+            var outputImage = image.Clone();
+            foreach (var idx in indices)
+            {
+                var box = boxes[idx];
+                string label = $"{classNames[classIds[idx]]}: {confidences[idx]:0.00}";
+                Cv2.Rectangle(outputImage, box, new Scalar(0, 255, 0), 2);
+                Cv2.PutText(outputImage, label, new Point(box.X, box.Y - 10), HersheyFonts.HersheySimplex, 0.5, new Scalar(0, 255, 0));
+            }
+
+            return outputImage;
+        }
+    }
+}
